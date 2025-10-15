66d0852ca7b1594d609f413e48ca105d342537aa
DarkSoulsMan <joaodarksouls3dasilva@gmail.com>
2025-10-15 18:40:38 -0400

    conectado com o front

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const cors = require('cors')
 const routes = require('./routes')
 const path = require('path');
 
 // Permite ler JSON no corpo das requisições
 const app = express();
 
+
+//Permir as requisições do Vite
+app.use(cors({
+    origin: 'http://localhost:5173', //<= Endereço Padrã do Vite
+}))
+
+
 // Configura a pasta "uploads" como pública
+app.use(express.json())///<= Permite JSON e uploads
 app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
 
